9ca575ae66e59f257c0950a260093c4d2fe6add9
Mirko Hrubik <mirkopepper@gmail.com>
2015-04-26 01:08:23 -0300

    changed project name and added some methods

--- probabilitiescalculator.h
+++ probabilitiescalculator.h
@@ -10,6 +10,7 @@ class ProbabilitiesCalculator
 public:
     ProbabilitiesCalculator();
     void calculate(const QVector< QPair<QString, int> > &data);
+    QVector< QPair<QString, double> > getProbabilities();
 private:
      QVector< QPair<QString, double> > probs;
 
