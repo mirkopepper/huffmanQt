e63aa9e8be10762a2f1011c4c2db6cc51c4d3039
Mirko Hrubik <mirkopepper@gmail.com>
2015-05-12 22:29:52 -0300

    Deleted commented methods and debuggers

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -69,72 +69,12 @@ void MainWindow::startTable()
     }
 }
 
-/*
-void MainWindow::updateTable(QVector< QPair<QString, QString> > solution)
-{
-
-    for (int j = 0; j < solution.size(); j++) {
-        int i = 0;
-        bool done = false;
-        while (i < probs.size() && !done) {
-            if ((probs.at(i).first) == (solution.at(j)).first) {
-                QTableWidgetItem *item = new QTableWidgetItem(solution.at(j).second);
-                ui->solutionTable->setItem(i,2, item);
-                QString code = solution.at(j).second;
-                int lenght = code.length();
-                QTableWidgetItem *item2 = new QTableWidgetItem(QString::number(lenght));
-                ui->solutionTable->setItem(i,3, item2);
-                done = true;
-            }
-            else
-                i++;
-        }
-    }
-}*/
-/*
-QVector< QPair<int, double> > MainWindow::lenghtData()
-{
-    QVector< QPair<int, double> > data;
-    for (int i = 0; i < probs.size(); i++) {
-        QTableWidgetItem *proba = ui->solutionTable->item(i, 1);
-        QTableWidgetItem *lenght = ui->solutionTable->item(i, 3);
-        QString probaText = (*proba).text();
-        QString lenghtText = (*lenght).text();
-        double p = probaText.toDouble();
-        int l = lenghtText.toInt();
-        data.append(qMakePair(l, p));
-    }
-    return data;
-}*/
-
 void MainWindow::on_startButton_clicked()
 {
     IA.start(filePath);
-
     this->orderOneSymbols = prob.calculate(IA.getColorCount(), IA.getTotalPixels());
-
-    //probs = prob.getProbabilities(); NO SE NECESITA MAS
-
-
     HA.calculateHuffman(orderOneSymbols);
-
-    for (int i = 0; i < orderOneSymbols.size(); i++) {
-        qDebug() << orderOneSymbols.at(i).getColors();
-        qDebug() << "PROBABILITY" << orderOneSymbols.at(i).getProbability();
-        qDebug() << "HUFFCODE" << orderOneSymbols.at(i).getHuffmanCode();
-    }
-    qDebug() << "antes de cargar los datos ORDEN 1";
     startTable();
-
-    /*for (int i = 0; i < symbCodification.size(); i++) {
-        for (int e = 0; e < symbCodification.at(i).first.size(); e++) {
-            qDebug() << symbCodification.at(i).first.at(e);
-        }
-        qDebug() << "CODIGO: " << symbCodification.at(i).second;
-    }*/
-
-    //updateTable(solution);
-    //HA.setData(this->lenghtData());
     ui->LBox->setText(QString::number(HA.getAverageLength()));
     ui->HBox->setText(QString::number(HA.getEntropy()));
     ui->nBox->setText(QString::number(HA.getPerformance()));
@@ -155,7 +95,6 @@ void MainWindow::on_extendButton_clicked()
     prob.extend(n, orderNSymbols);
 
     nExtensionWindow.show();
-    qDebug() << "antes de cargar los datos ORDEN "<<n;
     nExtensionWindow.loadData(orderNSymbols);
     nExtensionWindow.setWindowTitle("N-EXTENSION");
 
