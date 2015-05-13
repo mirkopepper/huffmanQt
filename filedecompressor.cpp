579810d94d93f8a4c9484ca372f84ff328f13a18
Andres Chardon <chardon.andres@gmail.com>
2015-05-13 03:07:29 -0300

    Compressor/Decompressor working (i think)

--- filedecompressor.cpp
+++ filedecompressor.cpp
@@ -15,6 +15,7 @@ FileDecompressor::~FileDecompressor()
 void FileDecompressor::decompress()
 {
     //Opens HZIP file
+    qDebug() << endl << endl;
     QString filePath;
     QFileDialog dialog;
     dialog.setFileMode(QFileDialog::AnyFile);
@@ -27,7 +28,7 @@ void FileDecompressor::decompress()
     //Now we need to separate HEADER from DATA
 
     QFile inputFile(filePath);
-    inputFile.open(QIODevice::ReadOnly);
+    inputFile.open(QIODevice::ReadOnly | QIODevice::Text);
 
     /* Check it opened OK */
     if(!inputFile.isOpen()){
@@ -38,24 +39,48 @@ void FileDecompressor::decompress()
     QTextStream inStream(&inputFile);
 
     /* Write the line to the file */
-
+    qDebug() << "????????????????????????????????";
     QString text = inStream.readAll();
-
 
     /* Close the file */
     inputFile.close();
 
-    QStringList list = text.split(" ");
+    QStringList list = text.split(" ",QString::SkipEmptyParts);
     this->width = list.at(0).toInt();
     this->height = list.at(1).toInt();
-    this->imagedata;
+    this->imagedata = list.at(3);
     QStringList codelist = list.at(2).split("#");
     this->header = codelist;
 
+    decodificate();
 }
 
 void FileDecompressor::decodificate()
 {
+    QString file = this->imagedata;
+    std::string filestring = file.toStdString();
+    char temp = filestring.at(0);
+    qDebug() << "EL PRIMER CHAR SIN DECODIFICAR ES" <<temp;
+    QChar temp2 = filestring.at(0);
+    qDebug() << "EL PRIMER QCHAR SIN CODIFICAR ES" <<temp2;
+    qDebug() << "EL UNICODE: " << temp2.unicode();
+    QChar temp3 = filestring.at(1);
+    qDebug() << "EL PRIMER QCHAR SIN CODIFICAR ES" <<temp3;
+    qDebug() << "EL UNICODE: " << temp3.unicode();
+
+    QString image;
+
+
+    for (int i = 0; i < file.size(); i++) {
+        char mander = filestring.at(i);
+        for (int e = 0; e < 16; e++) {
+            int value = (e >> mander) & 1;
+            qDebug() << value;
+            image.append(value);
+        }
+    }
+    qDebug() << image.at(0) << image.at(1);
+
 
 }
 
