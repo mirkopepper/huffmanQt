e20ec720ebcdc2fa3d5c90f52b609a2d9f1bb82e
Mirko Hrubik <mirkopepper@gmail.com>
2015-05-13 09:45:54 -0300

    Implemented methods to generate the decompressed image

--- filedecompressor.cpp
+++ filedecompressor.cpp
@@ -1,6 +1,6 @@
 #include "filedecompressor.h"
-
-
+#include <QMap>
+#include <QBuffer>
 
 FileDecompressor::FileDecompressor()
 {
@@ -49,23 +49,56 @@ void FileDecompressor::decompress()
     this->width = list.at(0).toInt();
     this->height = list.at(1).toInt();
     this->imagedata;
-    QStringList codelist = list.at(2).split("#");
-    this->header = codelist;
-
+    this->header = list.at(2).split("#", QString::SkipEmptyParts);
+    this->headerInterpreter();
+    //Tendria que quedar asi los bits que me da decodificar
+    //QString bits = decodificate();
+    QString bits = "1010111000010110101101011101010010101010010011110";
+    this->generateFile(bits);
 }
 
-void FileDecompressor::decodificate()
+QString FileDecompressor::decodificate()
 {
 
 }
 
 void FileDecompressor::headerInterpreter()
 {
-
+    QStringList::iterator it;
+    for(it=this->header.begin(); it!=this->header.end(); it++)
+    {
+        this->codes.insert(it->mid(6), it->mid(0,6).prepend("#"));
+    }
 }
 
-void FileDecompressor::generateFile()
+void FileDecompressor::generateFile(QString bits)
 {
+    QString aux;
+    QVector<QColor> colors;
+    QString::iterator it;
+    for(it=bits.begin(); it!=bits.end(); it++)
+    {
+        aux += *it;
+        if(this->codes.contains(aux))
+        {
+            colors.push_back(QColor(this->codes.find(aux).value()));
+            aux.clear();
+        }
+    }
+    qDebug() << colors;
+    QImage img;
+    QByteArray ba;
+    QBuffer buffer(&ba);
+    //Esto va a tardar por la resolucion de la imagen, no es que se colgo el programa
+    for(int i=0; i<this->height; i++)
+    {
+        for(int j=0; j<this->width; j++)
+        {
+            img.setPixel(i,j,colors.first().rgb());
+        }
 
+    }
+    //Tengo que ver como guardar la imagen
+    img.save(&buffer, "PNG");
 }
 
