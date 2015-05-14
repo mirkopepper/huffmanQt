8285b3153b906ba84137e0f2556a9834c3447933
Andres Chardon <chardon.andres@gmail.com>
2015-05-14 17:25:26 -0300

    About windows description changed

--- about.cpp
+++ about.cpp
@@ -7,7 +7,8 @@ About::About(QWidget *parent) :
 {
     ui->setupUi(this);
     QString textToShow = "Este programa fue realizado como trabajo de cursada para la materia Teoría de la Información por Mirko Hrubik y Andrés Chardon. \n";
-    textToShow.append("\n El objetivo del programa es, dada una imagen, obtener las probabilidades asociadas a cada color en ella, y su codificación Huffman asociada.");
+    textToShow.append("\n El objetivo del programa es, dada una imagen, obtener las probabilidades asociadas a cada color en ella, y su codificación Huffman asociada. \n");
+    textToShow.append("\n Luego, la imagen puede ser comprimida en un archivo '.hzip'. Finalmente, el archivo puede descomprimirse para generar de nuevo la misma imagen.");
     ui->infoBox->setText(textToShow);
 }
 
